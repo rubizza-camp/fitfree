349f461041837c730a86c81763a97e56b2e94d42
Lika Lex <lika.lex555@gmail.com>
2018-08-04 17:40:30 +0300

    rails_apps_composer: add Bootstrap page layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users
