2feb9c9873aa1a53aa43e147a796ad916cc605cc
Aleksei <Lazarenko1294@yandex.ru>
2018-08-06 18:55:11 +0300

    Fix data types

--- db/migrate/20180806145229_create_messages.rb
+++ db/migrate/20180806145229_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.int :user_id
-      t.int :client_id
+      t.integer :user_id
+      t.integer :client_id
       t.text :text
-      t.int :attachment_id
+      t.integer :attachment_id
 
       t.timestamps
     end
--- db/migrate/20180806154556_create_attachments.rb
+++ db/migrate/20180806154556_create_attachments.rb
@@ -1,8 +1,8 @@
 class CreateAttachments < ActiveRecord::Migration[5.2]
   def change
     create_table :attachments do |t|
-      t.int :message_id
-      t.varchar :path
+      t.integer :message_id
+      t.text :path
 
       t.timestamps
     end
