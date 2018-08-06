61c27bbc8c42ddfccd7b276fb70a4824accbde21
Lika Lex <lika.lex555@gmail.com>
2018-08-06 18:11:26 +0300

    Generate meal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_112537) do
+ActiveRecord::Schema.define(version: 2018_08_06_150628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_08_06_112537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.datetime "datetime"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "client_id"
   end
 
   create_table "trainings", force: :cascade do |t|
