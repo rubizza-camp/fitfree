33a73f18bf1c0d8297c00879583dc66a9b3b65a6
Equinox-01 <vereskovskystas@gmail.com>
2018-08-07 10:06:02 +0300

    Add Exercise and ExerciseSet models

---
+++ db/migrate/20180807070506_create_exercise_sets.rb
@@ -0,0 +1,10 @@
+class CreateExerciseSets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :exercise_sets do |t|
+      t.references :exercise, foreign_key: true
+      t.references :training, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
