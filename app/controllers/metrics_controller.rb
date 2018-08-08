d61e907fa993dc91e4b63b0f124130cd7ebc9628
Lika Lex <lika.lex555@gmail.com>
2018-08-08 13:07:49 +0300

    Views to simple_form.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,6 +1,21 @@
 class MetricsController < ApplicationController
+  before_action :find_metric, only: [:show]
   include ClientableControllerConcern
 
   def index
+    @metric = Metric.all.order('created_at DESC')
   end
+  def show
+
+  end
+
+  private
+  def metric_params
+    params.require(:metric).permit(:name, :unit)
+  end
+
+  def find_metric
+    @metric = Metric.find(params[:id])
+  end
+
 end
