037d8fecb5ead1fbf50010ad3e7c9b9402a15a23
Equinox-01 <vereskovskystas@gmail.com>
2018-08-14 12:50:28 +0300

    Add field price to client model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_104118) do
+ActiveRecord::Schema.define(version: 2018_08_14_092033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_08_09_104118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "price"
+    t.string "integer"
   end
 
   create_table "exercise_sets", force: :cascade do |t|
