b5c9bcc78800a9f9b7b1f9648bdfdfda527c25c1
Aleksei <Lazarenko1294@yandex.ru>
2018-08-17 15:48:28 +0300

    Empty commit

--- db/migrate/20180807092242_remove_type_from_meters.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromMeters < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :meters, :type, :string
-  end
-end
--- db/migrate/20180807195417_remove_meters.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMeters < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :meters
-    drop_table :amounts
-  end
-end
