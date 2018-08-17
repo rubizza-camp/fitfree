b2f81e1a48a16f3a5d3ffe194769fee0564ff297
Equinox-01 <vereskovskystas@gmail.com>
2018-08-17 13:49:40 +0300

    Add 2 hours training

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -57,10 +57,10 @@ class TrainingsController < ApplicationController
   def create_background_proc(training_id)
   # TODO: + 2 hours
   training = Training.find(training_id)
-  tmp = WithdrawPaymentJob.perform_at(training.time.to_f,
+  tmp = WithdrawPaymentJob.perform_at((training.time + 1.minutes).to_f,
                                       { :client_id => training.client_id,
                                         :user_id => training.user_id,
-                                        :time => training.time, # + 2.hours,
+                                        :time => training.time + 1.minutes,
                                         :price => training.price,
                                         :training_id => training.id
                                       }.to_json)
