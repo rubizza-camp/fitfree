b5d4efe7fefc6c8798c13fb5bfe42233207133d2
brutu33 <dmitrikotova3@gmail.com>
2018-08-22 11:35:58 +0300

    clear code in webhooks controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,28 +1,24 @@
+# The WebhooksController response for handling relegram bot webhook messages
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def callback
-    puts message[:text]
-    if client && client.messages.find_by(update_id: webhook[:update_id]).nil? && message[:text]
-      client.messages.create(text: message[:text], update_id: webhook[:update_id])
-      ActionCable.server.broadcast 'messages',
-                                   message: message[:text],
-                                   user: client.first_name
-      head :ok
+    if client && message_exist.nil? && message[:text]
+      client.messages.create(text: message[:text], update_id: web_hook[:update_id])
+      send_broadcast
     end
     render nothing: true, head: :ok
   end
 
-  def webhook
-    params['webhook']
+  def message_exist
+    client.messages.find_by(update_id: web_hook[:update_id])
   end
 
-  def message
-    webhook['message']
-  end
-
-  def from
-    message[:from]
+  def send_broadcast
+    ActionCable.server.broadcast 'messages',
+                                 message: message[:text],
+                                 user: client.first_name
+    head :ok
   end
 
   def client
@@ -30,10 +26,30 @@ class WebhooksController < ApplicationController
   end
 
   def find_by_binding_id
-    if message[:entities].present? && message[:entities].first[:type].eql?('bot_command')
-      text = message[:text][/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/]
-      @client = Client.find_by(telegram_bind_id: text) if text
-      @client.update(telegram_chat_id: from[:id]) if @client
+    if msg_entities.present? && msg_entities.first[:type].eql?('bot_command')
+      @client = Client.find_by(telegram_bind_id: binding_id) if binding_id
+      @client&.update(telegram_chat_id: from[:id])
     end
+  end
+
+  def binding_id
+    @binding_id ||= message[:text]
+    [/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/]
+  end
+
+  def web_hook
+    params['webhook']
+  end
+
+  def message
+    web_hook['message']
+  end
+
+  def msg_entities
+    message[:entities]
+  end
+
+  def from
+    message[:from]
   end
 end
