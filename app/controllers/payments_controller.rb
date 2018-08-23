58d78e9458f7760410702490234a3191237b5e51
Equinox-01 <vereskovskystas@gmail.com>
2018-08-23 15:51:56 +0300

    Update payment logic

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,12 +4,21 @@ class PaymentsController < ApplicationController
 
   def index
     client_id = params[:client_id]
-    @payments_list = Transaction.where(client_id: client_id).sort_by(&:datetime).reverse!
-                         .paginate(page: params[:page], per_page: 10)
+    @payments_list = Transaction.where(client_id: client_id).order(created_at: :desc)
+    @payments_list = @payments_list.paginate(page: params[:page], per_page: 10)
     @result_balance = Client.find(client_id).cash
   end
 
   def create; end
+
+  def delete
+    trans = Transaction.find(params[:id])
+    client = Client.find(params[:client_id])
+    client.add_to_cash(-trans.price)
+    client.save!
+    trans.delete
+    redirect_to "/clients/#{params[:client_id]}/payments"
+  end
 
   def add
     current_user.transactions.create(datetime: params[:date],
@@ -17,5 +26,6 @@ class PaymentsController < ApplicationController
                                      price: params[:price],
                                      user_id: current_user.id)
     Client.find(params[:client_id]).add_to_cash(params[:price])
+    redirect_to "/clients/#{params[:client_id]}/payments"
   end
 end
