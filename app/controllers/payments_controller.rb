83669eaad5e676a141d2fff60f0786b48d052e86
Equinox-01 <vereskovskystas@gmail.com>
2018-08-27 11:14:58 +0300

    Refactor payment_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,7 +3,6 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    client_id = params[:client_id]
     @payments_list = Transaction.where(client_id: client_id).order(created_at: :desc)
     @payments_list = @payments_list.paginate(page: params[:page], per_page: 10)
     @result_balance = Client.find(client_id).cash
@@ -12,20 +11,32 @@ class PaymentsController < ApplicationController
   def create; end
 
   def delete
-    trans = Transaction.find(params[:id])
-    client = Client.find(params[:client_id])
-    client.add_to_cash(-trans.price)
-    client.save!
-    trans.delete
-    redirect_to "/clients/#{params[:client_id]}/payments"
+    transaction = Transaction.find(params[:id])
+    add_cash(-transaction.price)
+    transaction.delete
+    redirect_to "/clients/#{client_id}/payments"
   end
 
   def add
     transaction = current_user.transactions.create(datetime: params[:date],
-                                     client_id: params[:client_id],
-                                     price: params[:price],
-                                     user_id: current_user.id)
-    Client.find(params[:client_id]).add_to_cash(params[:price]) if transaction.valid?
-    redirect_to "/clients/#{params[:client_id]}/payments"
+                                                   client_id: client_id,
+                                                   price: params[:price],
+                                                   user_id: current_user.id)
+    add_cash(transaction.price) if transaction.valid?
+    redirect_to "/clients/#{client_id}/payments"
+  end
+
+  private
+  def client_id
+    params[:client_id]
+  end
+
+  def client
+    @client ||= Client.find(client_id)
+  end
+
+  def add_cash(price)
+    client.add_to_cash(price)
+    client.save!
   end
 end
