0db39672d25b9345294630d3eda549a24d925f19
brutu33 <dmitrikotova3@gmail.com>
2018-08-29 09:33:36 +0300

    clear code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,44 +16,25 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require moment
-////= require fullcalendar
 
 ////= require select2/dist/js/select2.full.js
 //= require select2
 //= require chart.js/dist/Chart.js
-////= require_tree .
+//= require_tree .
 //= require cable
 
 //= require semantic-ui
 //= require semantic-ui/modal
 //= require semantic-ui/dropdown
 
-// config.action_cable.mount_path = '/messages';
 
-$(document).ready(function() {
-    $('#toc')
-        .sidebar({
-            dimPage          : true,
-            transition       : 'overlay',
-            mobileTransition : 'uncover'
-        }).sidebar('attach events', '.menu .item');
-    // check_window_size()
-});
-
-// $(window).resize(check_window_size);
-
-// function check_window_size() {
-//     if($(window).width() > 1199) {
-//         // $('.ui.sidebar').sidebar({
-//         //     context: $('.bottom.segment')
-//         // }).sidebar('show');
-//         $('.toc').css('display', 'flex');
-//         $('.attached.menu').css("display", "none")
-//     } else {
-//         $('.toc').css('display', 'none');
-//         // $('.ui.sidebar').sidebar({context: '.bottom.segment'});
-//         $('.attached.menu').css("display", "flex")
-//     }
-// }
+// $(document).ready(function() {
+//     $('#toc')
+//         .sidebar({
+//             dimPage          : true,
+//             transition       : 'overlay',
+//             mobileTransition : 'uncover'
+//         }).sidebar('attach events', '.menu .item');
+// });
 
 
