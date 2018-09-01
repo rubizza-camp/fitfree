ac282ae0021f8c82457c6dbea8355889bd4e4226
Aleksei <Lazarenko1294@yandex.ru>
2018-09-01 03:47:40 +0300

    Add whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,26 @@
-every 1.minute do
-  command "puts 'Here I am'"
+# Короче, я совсем перестал соображать, но вот что я выясни:
+# вроде как можно тут в кавычках после "runner" указать модель и метод этой модели,
+# типа runner "Messager.send_notification", но это не точно
+# Как это протестить, я хз ибо даже как запустить не понял.
+# Такие дела
+
+
+every 1.day, at: ['10:00 am'] do #breakfast
+  runner ""
+end
+
+every 1.day, at: ['12:00 pm'] do #perekus
+  runner ""
+end
+
+every 1.day, at: ['3:00 pm'] do #lunch
+  runner ""
+end
+
+every 1.day, at: ['5:00 pm'] do #perekus
+  runner ""
+end
+
+every 1.day, at: ['8:00 pm'] do #dinner
+  runner ""
 end
